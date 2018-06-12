b34455f0221dbbef9ee2e136d8349d54a35448a1
AaditT <33333433+AaditT@users.noreply.github.com>
2018-06-12 00:46:36 -0700

    Update pic.py

--- examples/pic.py
+++ examples/pic.py
@@ -1,20 +1,17 @@
 import braille
 import cv2
 
-
 def imgShow():
     cam = cv2.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         cv2.imshow('Detect Image', img)
         if cv2.waitKey(1) == 27:
+            print("image taken")
             cv2.imwrite("test.jpg",img)
-        else: 
+            if braille.imageToBraille("test.jpg") == "":
+                print("Image not recognized")
             break
     cv2.destroyAllWindows()
 
-def detectImage():
-    imgShow()
-    braille.imageToBraille("test.jpg")
-
-detectImage()
+imgShow()
